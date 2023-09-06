0692628384bded4d78b8338fdffd3317351bb62a
lingxiao <13656261355@163.com>
2023-09-06 14:04:05 +0800

    set up backgroundColor

--- 02-WindowAndSplit/02-WindowAndSplit/PrimaryVireController.swift
+++ 02-WindowAndSplit/02-WindowAndSplit/PrimaryVireController.swift
@@ -8,12 +8,17 @@
 import Cocoa
 
 class PrimaryVireController: NSViewController {
-
+    
     @IBOutlet weak var primaryBtn: NSButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do view setup here.
+        
+        // 设置背景色为红色
+        view.wantsLayer = true // 必须启用 wantsLayer 属性
+        view.layer?.backgroundColor = NSColor.red.cgColor
+                
     }
     
     @IBAction func btnAction(_ sender: Any) {
